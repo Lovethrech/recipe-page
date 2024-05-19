67d5b0ee9d088e1c53b5d5cce41ae4d53f624fb6
Lovethrech <rachealdolapo45@gmail.com>
2024-05-19 16:57:28 +0100

    working on the main-hero component

--- recipe-web/src/App.vue
+++ recipe-web/src/App.vue
@@ -1,5 +1,19 @@
+<script setup>
+import MainHero from "@/components/MainHero.vue";
+import Ingredients from "@/components/Ingredients.vue";
+</script>
+
+<style scoped>
+.app-main{
+  background-color: hsl(0, 0%, 100%);
+  width:50vw;
+  padding:20px 50px;
+}
+</style>
+
 <template>
-
-
+  <div class="app-main">
+    <MainHero></MainHero>
+    <Ingredients></Ingredients>
   </div>
 </template>
---
+++ recipe-web/src/components/MainHero.vue
@@ -0,0 +1,49 @@
+<style scoped>
+.main-hero{
+
+}
+.main-hero-image-ctn{
+    width:100%;
+    height:100%;
+}
+.main-hero-image-ctn img{
+    width:100%;
+    height:100%;
+}
+.main-hero-text-section h1{
+    font-weight:700;
+    font-size:30px;
+}
+.hero-title-text{
+    font-size: 16px;
+    line-height:1.5
+}
+.prep-time{
+    background-color: hsl(330, 100%, 98%);
+    padding:30px 50px;
+    line-height:2;
+}
+</style>
+
+<template>
+    <div class="main-hero">
+        <div class="main-hero-image-ctn">
+            <img src="/public/image-omelette.jpeg" alt="Hero Omelette image">
+        </div><br/>
+        <div class="main-hero-text-section"><br>
+            <h1>Simple Omelette Recipe</h1><br>
+            <p class="hero-title-text">
+                An easy and quick dish, perfect for any meal. This classic omelette combines
+                beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats
+            </p><br><br>
+            <div class="prep-time">
+                <h2>Preparation time</h2>
+                <ul>
+                    <li><b>Total</b>: Approximately 10 minutes</li>
+                    <li><b>Preparation</b>: 5 minutes</li>
+                    <li><b>Cooking</b>: 5 minutes</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
